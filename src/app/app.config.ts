1c9a996285da3d1794a229483a36f4ed4f0b7bbf
AlinaSyniavska <alina22syniavska@gmail.com>
2025-04-14 16:58:22 +0300

    add routing setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,15 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+import { provideRouter, withComponentInputBinding, withViewTransitions } from '@angular/router';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
+import { routes } from './app.routes';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [ provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(
+      routes,
+      withViewTransitions(), // Додає анімацію переходів між сторінками
+      withComponentInputBinding() // Дозволяє передавати параметри маршруту як @Input() в компоненти
+    ),
+    provideClientHydration(withEventReplay()) ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  // Lazy loading для сторінки з продуктами
+/*  {
+    path: 'products',
+    loadComponent: () => import('./products/products.component').then(m => m.ProductsComponent)
+  },
+  // Lazy loading для цілого модуля
+  {
+    path: 'admin',
+    loadChildren: () => import('./admin/admin.routes').then(r => r.ADMIN_ROUTES)
+  },
+  { path: '**', component: NotFoundComponent }*/
+];
