f8c16abd300204016aca2bf559054ce25a535e24
AlinaSyniavska <alina22syniavska@gmail.com>
2025-04-22 22:17:22 +0300

    add reaction picker functionality with selection display and styling

--- src/app/components/reaction-picker/reaction-picker.component.ts
+++ src/app/components/reaction-picker/reaction-picker.component.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, input, linkedSignal } from '@angular/core';
 
 @Component({
   selector: 'app-reaction-picker',
@@ -8,5 +8,16 @@ import { Component, input } from '@angular/core';
   styleUrl: './reaction-picker.component.css'
 })
 export class ReactionPickerComponent {
-  reactions = input();
+  reactions = input<string[]>([]);
+
+  selectedReaction = linkedSignal<string[], string | null>({
+    source: () => this.reactions(),
+    computation: (source, previous) => {
+      return source.find(r => previous?.value === r) || null;
+    }
+  });
+
+  protected isSelected(reaction: string) {
+    return this.selectedReaction() === reaction;
+  }
 }
